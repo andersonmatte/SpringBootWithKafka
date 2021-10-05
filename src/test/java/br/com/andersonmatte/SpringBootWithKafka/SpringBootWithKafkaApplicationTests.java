c484b259d7766cae156f0a73bea9030b8f32532f
atamborim <anderson.tamborim@gertec.com.br>
2021-10-05 11:07:04 -0300

    add spring project

---
+++ src/test/java/br/com/andersonmatte/SpringBootWithKafka/SpringBootWithKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.andersonmatte.SpringBootWithKafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
